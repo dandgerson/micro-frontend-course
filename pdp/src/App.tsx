30dd904c825fcd281cff0dac470b4e73759a8ad3
Dmitry G. Anderson <dandgerson@gmail.com>
2022-05-19 23:54:24 +0300

    init api

--- pdp/src/App.tsx
+++ pdp/src/App.tsx
@@ -4,13 +4,10 @@ import Footer from "home/Footer";
 import Header from "home/Header";
 
 import "./index.scss";
-import SafeComponent from "./SafeComponent";
 
 const App = () => (
   <div className="text-3xl mx-auto max-w-6xl">
-    <SafeComponent>
-      <Header />
-    </SafeComponent>
+    <Header />
     <div className='my-10'>PDP Page Content</div>
 
     <Footer />
